f3d53bdefc5a366424fd8a8193990aefe9d0a2e0
unknown <91155109+rajabali-ai@users.noreply.github.com>
2023-07-23 14:13:18 +0500

    Browse topic filter added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from .models import Room
+from .models import Room, Topic
 from .forms import RoomForm
 
 
@@ -14,8 +14,10 @@ from .forms import RoomForm
 
 
 def home(request):
-    rooms = Room.objects.all() 
-    context = {'rooms': rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else '' 
+    rooms = Room.objects.filter(topic__name__contains= q)
+    topics = Topic.objects.all()
+    context = {'rooms': rooms, 'topics': topics}
     return render(request, 'base/home.html', context)
 
 
