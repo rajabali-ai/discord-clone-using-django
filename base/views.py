0a69e5192db0885902ba61c67f6129a3ab9f634f
unknown <91155109+rajabali-ai@users.noreply.github.com>
2023-07-24 13:37:12 +0500

    search with keywords added

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 # from django.http import HttpResponse
 from .models import Room, Topic
 from .forms import RoomForm
+from django.db.models import Q
 
 
 
@@ -15,9 +16,14 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else '' 
-    rooms = Room.objects.filter(topic__name__contains= q)
+    rooms = Room.objects.filter(
+        Q(topic__name__contains= q) |
+        Q(name__icontains=q)  |
+        Q(description__icontains=q)
+          )
     topics = Topic.objects.all()
-    context = {'rooms': rooms, 'topics': topics}
+    room_count = rooms.count()
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 
