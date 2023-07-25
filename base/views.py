fbb84fe70db84a0eaf3e0b44f76d5c5995e5ab4a
unknown <91155109+rajabali-ai@users.noreply.github.com>
2023-07-25 13:50:56 +0500

    User registration added

--- base/views.py
+++ base/views.py
@@ -8,6 +8,7 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 
 
 
@@ -21,11 +22,13 @@ from django.http import HttpResponse
 
 def loginPage(request):
 
+    page = 'login'
+
     if request.user.is_authenticated:
         return redirect('home')
 
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
 
         try:
@@ -46,13 +49,31 @@ def loginPage(request):
         # else:
         #     messages.error(request, "Incorrect password.")
 
-    context = {}
+    context = {'page': page}
     return render(request, 'base/login_register.html', context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home') 
+        else:
+            messages.error(request, "An error occurred during registration.")
+
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else '' 
