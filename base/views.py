d3b3f8c85b2968f25072b9c1a9e8e9004c5b8aae
unknown <91155109+rajabali-ai@users.noreply.github.com>
2023-08-01 21:30:39 +0500

    Seeing messages and Sending  message in Chatroom added

--- base/views.py
+++ base/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 # from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.db.models import Q
 from django.contrib.auth.models import User
@@ -96,7 +96,17 @@ def room(request,pk):
     # context = {'room':room}
 
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('created')
+
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room= room,
+            body=request.POST.get('body')
+        )
+        return redirect('room',pk=room.id)
+
+    context = {'room': room, 'room_messages': room_messages}
 
     return render(request, 'base/room.html', context)
 
